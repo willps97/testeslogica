1b3fba1d8c52f3a5e0e14990bee98d64c88058f4
William Santos <william.sanctos@gmail.com>
2022-01-29 16:50:45 -0300

    Exercicio para o treinamento do laço while

---
+++ Exercicio_senha/Exercicio_senha/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio_senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        //Escreva um programa que repita a leitura de uma senha até que ela seja válida.Para cada leitura de senha 
+        //incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser 
+        //impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+
+        {
+            Console.WriteLine("Digite a sua senha de 4 digitos: ");
+            double pw = double.Parse (Console.ReadLine());
+
+            while(pw != 2002) {
+                Console.WriteLine("Senha inválida. Digite novamente por favor: ");
+                pw = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Senha correta! Acesso liberado.");
+            
+        }
+    }
+}
