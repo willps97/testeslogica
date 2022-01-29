84d83f892b605e9252b0479273e4ddd8591c7fdb
William Santos <william.sanctos@gmail.com>
2022-01-29 16:45:13 -0300

    Exercicio de treinamento para uso do laço for!

---
+++ ExercicioFor_2/ExercicioFor_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExercicioFor_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int SomaIn = 0;
+            int SomaOut = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if(x >= 10 && x<= 20)
+                {
+                    SomaIn =SomaIn+1 ;
+                    
+                }
+                else
+                {
+                    SomaOut =SomaOut+1;
+                   
+                }
+               
+            }
+            Console.WriteLine(SomaIn + " in");
+            Console.WriteLine(SomaOut + " Out");
+
+        }
+    }
+}
